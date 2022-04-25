ae08d3abdb7e40dda0e5f2efcf912551d3d71b9c
souris-dev <suorees113@gmail.com>
2022-04-25 11:11:23 +0530

    Fixed message handler

--- docs/_includes/js/custom.js
+++ docs/_includes/js/custom.js
@@ -1,8 +1,8 @@
-window.addEventListener("theme-switch", (event) => {
-  if (event.data == "dark") {
+window.addEventListener("message", (event) => {
+  if (event.data == "theme-switch-dark") {
     jtd.setTheme("dark");
   }
-  else if (event.data == "light") {
+  else if (event.data == "theme-switch-light") {
     jtd.setTheme("light");
   }
 }, false);
